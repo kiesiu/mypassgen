883d5dd972897e63be9e1d208bd0f94d30883ed4
Łukasz Kieś <kiesiu@pld-linux.org>
2015-02-10 23:41:34 +0100

    Update RemoteViews in AppWidget.

--- app/src/main/java/com/kiesiu/mypassgenwidget/AppWidget.java
+++ app/src/main/java/com/kiesiu/mypassgenwidget/AppWidget.java
@@ -1,9 +1,16 @@
 /*
- * Copyright 2015 Łukasz Kieś
+ * Copyright 2015 Łukasz Kieś <kiesiu@kiesiu.com>.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.kiesiu.mypassgenwidget;
@@ -11,12 +18,16 @@ package com.kiesiu.mypassgenwidget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.widget.RemoteViews;
 
 
 public class AppWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
     }
 
     @Override
@@ -25,5 +36,11 @@ public class AppWidget extends AppWidgetProvider {
 
     @Override
     public void onDisabled(Context context) {
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 }
