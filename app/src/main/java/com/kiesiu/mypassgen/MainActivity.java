81210c4506e7d80031ea4d25cb450b012ee44a5d
Łukasz Kieś <kiesiu@pld-linux.org>
2015-02-14 23:56:10 +0100

    Simple menu implementation for MainActivity.

--- app/src/main/java/com/kiesiu/mypassgen/MainActivity.java
+++ app/src/main/java/com/kiesiu/mypassgen/MainActivity.java
@@ -16,7 +16,11 @@
 package com.kiesiu.mypassgen;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
@@ -24,5 +28,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_rate:
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=com.kiesiu.mypassgen")));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
